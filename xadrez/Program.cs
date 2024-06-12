f43ff5d704e51e15e76d2e5da2c1251d33341ced
atilaVasck <139605662+AtilaVasck@users.noreply.github.com>
2024-06-12 15:57:32 -0300

    Add files via upload

---
+++ xadrez/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using tabuleiro;
+using jogo;
+
+namespace xadrez
+{
+    class Program
+    {
+        static void Main(string[] args){
+            PartidaDeXadrez partida = new PartidaDeXadrez();
+
+            while( partida.terminada != true ) {
+
+                Console.Clear();
+                Tela.imprimeTabuleiro( partida.tab );
+
+
+                Posicao origem  = Tela.lerPosicaoXadrez().toPosicao();
+                Console.Clear();
+
+                bool[,] posicoesPossiveis = partida.tab.peca( origem ).movimentosPossiveis();
+
+                Tela.imprimeTabuleiro( partida.tab , posicoesPossiveis );
+
+                Posicao destino = Tela.lerPosicaoXadrez().toPosicao();
+
+                if( posicoesPossiveis[ destino.linha, destino.coluna ]) {
+                    partida.executaMovimento( origem , destino );
+                } else {
+                    Console.Write( "Jogada Invalida!!!" );
+                }
+
+                
+
+            }
+            
+            // teste posição dublicada
+            // tabuleiro.colocarPeca( new Torre( tabuleiro , Cor.Preta ) , new Posicao( 5 , 6 ) );
+
+            // teste posição invalida
+            // tabuleiro.colocarPeca( new Torre( tabuleiro , Cor.Preta ) , new Posicao( 9 , 9 ) );
+            // Tela.imprimeTabuleiro(tabuleiro);
+
+            // PosicaoXadrez posicao = new PosicaoXadrez( 'h' , 3 );
+
+
+            // Tela.imprimeTabuleiro( tabuleiro );
+            
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ xadrez/tabuleiro/Peca.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace tabuleiro
+{
+    abstract class Peca {
+
+        public Posicao posicao  { get; set; }
+        public Cor cor          { get; protected set; }
+        public int qtMovimentos { get; protected set; }
+        public Tabuleiro tab    { get; protected set; }
+
+        public Peca(Tabuleiro tab , Cor cor ){
+
+            this.posicao = null;
+            this.tab = tab;
+            this.cor = cor;
+            this.qtMovimentos = 0;
+        }
+
+        public void incrementarQteMovimentos() {
+            this.qtMovimentos++;
+        }
+
+        public abstract bool[,] movimentosPossiveis();
+
+
+    }
+}
